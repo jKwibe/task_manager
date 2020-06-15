c5284b54cd34bfc31bab0360fed6ab4085a0c234
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-06-15 16:39:26 -0600

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/tasks',  to: 'tasks#index'
   get '/tasks/new', to: 'tasks#new'
+  get '/tasks/:id/edit', to: 'tasks#edit'
   get '/tasks/:id', to: 'tasks#show'
 
   post 'tasks', to: 'tasks#create'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
